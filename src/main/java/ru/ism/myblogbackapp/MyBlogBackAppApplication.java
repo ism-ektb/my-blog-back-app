195f7892cf3beb992137905be9413be0ea4e0fca
Ilya <ism-ektb@yandex.ru>
2026-02-15 09:54:48 +0500

    first commit

---
+++ src/main/java/ru/ism/myblogbackapp/MyBlogBackAppApplication.java
@@ -0,0 +1,13 @@
+package ru.ism.myblogbackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogBackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogBackAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ism/myblogbackapp/MyBlogBackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ism.myblogbackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogBackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
